4d4c016e32cc691dae8f29273745103222f20c59
Deepikaramesh-12 <113659376+Deepikaramesh-12@users.noreply.github.com>
2024-07-22 15:22:56 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend developer", "Python developer", "Full Stack developer"],
+    strings: ["Frontend developer", "Python developer", "Full Stack developer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
